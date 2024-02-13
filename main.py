da5ceff768361f5782f11b7b795fd56fe32f7097
Your Name <you@example.com>
2024-02-13 20:45:03 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("First commit")
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+menu = ["Главная", "Галерея","О нас"]
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html" ,menu=menu, title = "Главная")
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "О компании")
+
+if __name__ == "__main__":
+    app.run(debug=True )
