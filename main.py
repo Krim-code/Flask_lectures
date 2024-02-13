61f6022d7892b497e23fba1312d016d3aa0e608e
Your Name <you@example.com>
2024-02-13 21:32:13 +0300

    test

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
-menu = ["Главная", "Галерея","О нас"]
+menu = [
+            {"name":"Главная","url":"/"},
+            {"name":"О нас","url":"/about"},
+        ]
 @app.route("/")
 @app.route("/index")
 def index():
+    print(url_for('index'))
     return render_template("index.html" ,menu=menu, title = "Главная")
 
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title = "О компании")
+    print(url_for('about'))
+    return render_template("about.html", title = "О компании", menu=menu)
+
+@app.route("/profile/<path:username>")
+def profile(username):
+    print(url_for('profile', username = "11"))
+    return f"Пользователь -- {username}"
 
 if __name__ == "__main__":
     app.run(debug=True )
